1ceab3915122a40c3c5be1ef093525f87efb1a15
Nikita <mn121288@gmail.com>
2018-01-07 13:17:42 +0300

    Рабочий вариант скрипта. Можно будет добавить переименование других типов файлов

---
+++ nav_obs_Rename.py
@@ -0,0 +1,28 @@
+﻿import os
+i = 0
+
+s = int(input('Введите количество файлов'))
+while(i <= s)://all files cycle
+    i=i+1
+    navFiles = []
+    files = []//files array creating
+
+    files = os.listdir(os.curdir)//making list of files in dir
+    print(files)
+
+    prevName = files[1]
+//working dir set
+    dir_work = os.listdir(os.curdir)
+    a = len (dir_work)
+    print (a)
+    if (prevName[-3:-1] == 'na')://searching nav-file
+            secondName = 'sampleFolder/' + prevName[0:-4] + '.N'
+            os.rename(prevName,secondName)//rename nav-file
+
+    if (prevName[-3:-1] == 'ob')://searching obs-file
+            secondName = 'sampleFolder/' + prevName[0:-4] + '.O'
+            os.rename(prevName,secondName)//rename obs-file
+
+#Можно будет добавить ещё другие информационные сообщения (.R - глонасс и .G)
+
+
